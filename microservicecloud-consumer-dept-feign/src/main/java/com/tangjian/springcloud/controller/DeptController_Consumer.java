59bbf7488f2f742d54899ef48b2e26549ddd6694
TangShy <975751290@qq,com>
2019-09-01 22:48:52 +0800

    add feign

--- microservicecloud-consumer-dept-feign/src/main/java/com/tangjian/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-feign/src/main/java/com/tangjian/springcloud/controller/DeptController_Consumer.java
@@ -1,6 +1,7 @@
 package com.tangjian.springcloud.controller;
 
 import com.tangjian.springcloud.entities.Dept;
+import com.tangjian.springcloud.service.DeptClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,26 +18,31 @@ public class DeptController_Consumer {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    DeptClientService service;
+
     @RequestMapping(value = "/consumer/dept/add")
     public boolean add(Dept dept) {
-        return restTemplate.postForObject(REST_URL_PREFIX+"/dept/add", dept, Boolean.class);
+        return service.add(dept);
     }
 
     @RequestMapping(value = "/consumer/dept/get/{id}")
     public Dept get(@PathVariable("id") Long id) {
-        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/get/"+id, Dept.class);
+        return service.get(id);
     }
 
     @SuppressWarnings("unchecked")
     @RequestMapping(value = "/consumer/dept/list")
     public List<Dept> list() {
-        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list", List.class);
+        return service.list();
     }
 
-    //测试@EnableDiscoveryClient,消费端可以调用服务发现
-    @RequestMapping(value = "/consumer/dept/discovery")
-    public Object discovery() {
-        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/discovery", Object.class);
-    }
+//
+//    //测试@EnableDiscoveryClient,消费端可以调用服务发现
+//    @RequestMapping(value = "/consumer/dept/discovery")
+//    public Object discovery() {
+//        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/discovery", Object.class);
+//
+//    }
 
 }
